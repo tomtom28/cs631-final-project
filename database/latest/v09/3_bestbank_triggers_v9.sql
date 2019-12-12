2c8cd625103c5dfe9f0a76def4fa32ce95a5ae27
Tommy Thompson <tomthompson828@gmail.com>
2019-12-11 23:32:49 -0500

    added v9 DB updates to better match ER diagram... TODO: more triggers to update Charge_Account balance ... and make a trigger (or event) to update Branch Assets

---
+++ database/latest/v09/3_bestbank_triggers_v9.sql
@@ -0,0 +1,68 @@
+-- BEST BANK DATABASE TRIGGERS
+USE bestbank;
+
+-- Do some clean up:
+DROP TRIGGER IF EXISTS `update_account_balance_after_transaction_insert`;
+DROP TRIGGER IF EXISTS `update_account_balance_after_transaction_update`;
+DROP TRIGGER IF EXISTS `update_account_balance_after_transaction_deletion`;
+
+-- Trigger 1
+-- Updates the balance field of the given account whose
+-- newly inserted transaction has a matching account_no
+DELIMITER ;;
+CREATE TRIGGER update_account_balance_after_transaction_insert 
+AFTER INSERT ON transaction 
+FOR EACH ROW
+	IF NEW.transaction_code NOT IN ('BF') 
+    THEN
+		UPDATE account a
+		SET balance = (	SELECT sum(credit) - sum(debit)
+						FROM account_passbooks ap
+						WHERE ap.account_no = NEW.account_no)
+		WHERE a.account_no = NEW.account_no;
+	END IF;;
+DELIMITER ;
+
+
+
+-- Trigger 2
+-- Updates the balance field of the given account whose
+-- updated transaction has a matching account_no
+DELIMITER ;;
+CREATE TRIGGER update_account_balance_after_transaction_update
+AFTER UPDATE ON transaction 
+FOR EACH ROW
+	IF NEW.transaction_code NOT IN ('BF') 
+    THEN
+		UPDATE account a
+		SET balance = (	SELECT sum(credit) - sum(debit)
+						FROM account_passbooks ap
+						WHERE ap.account_no = NEW.account_no)
+		WHERE a.account_no = NEW.account_no;
+	END IF;;
+DELIMITER ;
+
+
+
+-- Trigger 3
+-- Updates the balance field of the given account whose
+-- deleted transaction has a matching account_no
+DELIMITER ;;
+CREATE TRIGGER update_account_balance_after_transaction_deletion 
+AFTER DELETE ON transaction 
+FOR EACH ROW
+	IF OLD.transaction_code NOT IN ('BF') 
+    THEN
+		UPDATE account a
+		SET balance = (	SELECT sum(credit) - sum(debit)
+						FROM account_passbooks ap
+						WHERE ap.account_no = OLD.account_no)
+		WHERE a.account_no = OLD.account_no;
+	END IF;;
+DELIMITER ;
+
+
+-- Note that Balance Forward transactions are exempt from these triggers
+-- since the account balance is directly inserted by the application        
+        
+        
