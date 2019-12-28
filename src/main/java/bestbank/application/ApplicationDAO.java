990a01a889f746e5f17f9b2bc3d4bc21b8bf6a32
Tommy Thompson <tomthompson828@gmail.com>
2019-12-27 20:49:59 -0500

    added JAWSDB functionality for heroku

--- src/main/java/bestbank/application/ApplicationDAO.java
+++ src/main/java/bestbank/application/ApplicationDAO.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import static java.lang.System.*;
+
 public class ApplicationDAO {
 
     // Database Credentials
@@ -14,10 +16,17 @@ public class ApplicationDAO {
     // Gets DB Connection Instance
     public static Connection getDBConnection() {
         try {
-            return DriverManager.getConnection(dataBaseURL, userName, password);
+            // Determine if connected to localhost or heroku
+            String JAWSDB_MARIA_URL = System.getenv("JAWSDB_MARIA_URL");
+            if (JAWSDB_MARIA_URL != null) { // deployed
+                return DriverManager.getConnection(JAWSDB_MARIA_URL);
+            }
+            else { // localhost
+                return DriverManager.getConnection(dataBaseURL, userName, password);
+            }
         }
         catch (SQLException e) {
-            System.out.println(e);
+            out.println(e);
             return null;
         }
     }
